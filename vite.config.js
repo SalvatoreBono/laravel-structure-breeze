9a08d3795bdf169f95c1980b25fd967a583778bc
Salvatore <salvatorebono2001@gmail.com>
2023-10-06 17:17:30 +0200

    first commit

---
+++ vite.config.js
@@ -0,0 +1,22 @@
+import laravel from 'laravel-vite-plugin';
+import path from "path";
+import {
+    defineConfig
+} from 'vite';
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            // edit the first value of the array input to point to our new sass files and folder.
+            input: ['resources/scss/app.scss', 'resources/js/app.js'],
+            refresh: true,
+        }),
+    ],
+    // Add resolve object and aliases
+    resolve: {
+        alias: {
+            '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+            '~resources': '/resources/'
+        }
+    }
+});
