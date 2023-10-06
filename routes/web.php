704151849a51013d1b627785a9bf5607da5d07ee
Salvatore <salvatorebono2001@gmail.com>
2023-10-06 21:42:47 +0200

    add partials and comments on web.php

--- routes/web.php
+++ routes/web.php
@@ -29,3 +29,25 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__ . '/auth.php';
+
+////////////////////////////////////////////ESEMPI////////////////////////////////////////////
+
+//Genera tutte le Route
+// Route::resource("comics", ComicController::class);
+
+// ROTTE CRUD COMICS
+
+// CREATE
+//Route::get('/comics/create', [ComicController::class, "create"])->name("comics.create");
+//Route::post('/comics', [ComicController::class, "store"])->name("comics.store");
+
+// READ
+//Route::get('/comics', [ComicController::class, "index"])->name("comics.index");
+//Route::get('/comics/{id}', [ComicController::class, "show"])->name("comics.show");
+
+// UPDATE
+//Route::get('/comics/{id}/edit', [ComicController::class, "edit"])->name("comics.edit");
+//Route::put('/comics/{id}', [ComicController::class, "update"])->name("comics.update");
+
+// DESTROY
+//Route::delete('/comics/{id}', [ComicController::class, "destroy"])->name("comics.destroy");
